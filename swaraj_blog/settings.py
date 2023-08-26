4975fa64e7eaf5b7e621f33db5cfc5454e435030
Swaraj <swarajnayakdipu227@gmail.com>
2023-08-26 05:30:19 +0530

    image uplode

--- swaraj_blog/settings.py
+++ swaraj_blog/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'swaraj_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'NkOup0mc2G66DxZcz5sG',
+        'HOST': 'containers-us-west-48.railway.app',
+        'PORT':'7588',
     }
 }
 
